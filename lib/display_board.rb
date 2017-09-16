22175da2d1caab47e0b13fb78aa2471474516210
Anthony Fields <janthonyfields@gmail.com>
2017-09-16 03:55:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,12 @@
 def display_board
   horiz = "-----------"
   three_space = "   "
-  cells = puts "#{three_space}|#{three_space}|"
-  line = puts "#{horiz}"
-  # puts "#{three_space}|#{three_space}|"
-  # puts "#{horiz}"
-  puts cells
-  puts line
-  puts cells
-  puts line
-  puts cells
+  # cells = puts "#{three_space}|#{three_space}|"
+  # line = puts "#{horiz}"
+  puts "#{three_space}|#{three_space}|"
+  puts "#{horiz}"
+  puts "#{three_space}|#{three_space}|"
+  puts "#{horiz}"
+  puts "#{three_space}|#{three_space}|"
 
 end
