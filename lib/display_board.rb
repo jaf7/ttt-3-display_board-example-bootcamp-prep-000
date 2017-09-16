17933cdc48cef707bbda5f01934c9eaf8479bc29
Anthony Fields <janthonyfields@gmail.com>
2017-09-16 03:35:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  vert = "|"
+  horiz = "-----------"
+  horiz_space = "   "
+
+  print "#{horiz_space}#{vert}#{horiz_space}"
+  print "#{horiz_space}#{vert}#{horiz_space}"
+  print horiz
+
+end
